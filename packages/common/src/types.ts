37454dfc5b9b3e3235ca36a3293bdefa2418613c
gamandeepsingh <gamandeepsingh4@gmail.com>
2025-04-24 21:28:55 +0530

    feat: initialize Prisma schema and client setup
    - Added Prisma schema definition for User, Room, and Chat models.
    - Generated TypeScript definitions for Prisma client.
    - Implemented Prisma client in the index file for database interactions.
    - Configured TypeScript settings for the database package.

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-    username : z.string().min(1).max(20),
+    email : z.string().email(),
+    username: z.string().min(1).max(20),
     password : z.string().min(8).max(20),
     name: z.string().min(1).max(20),
 })
 
 export const signInSchema = z.object({
-    username : z.string().min(1).max(20),
+    email : z.string().email(),
     password : z.string().min(8).max(20),
 })
 
